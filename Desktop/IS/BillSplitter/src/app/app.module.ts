88a7765b8e381573a8d715ae4afb4239b8adf327
Frankie Delos Santos <frankie@Frankies-MacBook-Pro-2.local>
2018-12-18 19:26:08 -0800

    finished lab7

--- Desktop/IS/BillSplitter/src/app/app.module.ts
+++ Desktop/IS/BillSplitter/src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 
+
+//angular material
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import{MaterialDesignModule} from '../material-design/material-design.module';
+
+
+//routing material
+import {AppRoutes} from './app.routes';
+import { HomeComponent } from './home/home.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MaterialDesignModule,
+    AppRoutes
   ],
   providers: [],
   bootstrap: [AppComponent]
